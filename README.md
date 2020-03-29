bfb0bd7ae898134cb1cc1e0894ffb0961576648c
Niweera <w.nipuna@gmail.com>
2020-03-29 19:02:08 +0530

    add upload files to aws s3

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # men-simple
+
 Most simple implementation of NodeJS, ExpressJS and Mongoose with EJS template engine.
+
+## Updates
+
+1. Upload file to S3 bucket and view it.
--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const AWS = require("aws-sdk");
+const multer = require("multer");
 
 const app = express();
+
+// Enter copied or downloaded access ID and secret key here
+const accessKeyId = "YOUR_ACCESS_KEY_ID";
+const secretAccessKey = "YOUR_SECRET_ACCESS_KEY";
+
+// The name of the bucket that you have created
+const BUCKET_NAME = "YOUR_BUCKET_NAME";
+
+// Initialize S3 bucket
+const s3 = new AWS.S3({
+  accessKeyId,
+  secretAccessKey
+});
+
+// set the multer memory storage
+const storage = multer.memoryStorage();
+
+// setting the memory storage as the multer storage
+// in here we don't save the file to disk
+// we save it in the memory and then we use the memory buffer
+// which will be uploaded to AWS S3 bucket
+const upload = multer({
+  storage
+});
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -78,6 +104,36 @@ app.post("/add-name", (req, res) => {
     .catch(() => {
       res.redirect("/");
     });
+});
+
+// render upload file web page
+app.get("/upload", (req, res) => {
+  res.render("upload", {
+    imageURL: null
+  });
+});
+
+// upload file
+app.post("/upload", upload.single("image"), (req, res) => {
+  const file = req.file;
+
+  // Setting up S3 upload parameters
+  const params = {
+    Bucket: BUCKET_NAME,
+    ContentType: file.mimetype, // file mimetype image/jpg image/png etc.
+    Key: `${file.originalname}.${new Date().getTime()}`, // File name and current time
+    Body: file.buffer, // file blob (This is what we upload
+    ACL: "public-read", // this is used to tell that the imageURL is public
+    ContentLength: file.size
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function(err, data) {
+    if (err) {
+      res.render("upload", { imageURL: null });
+    }
+    res.render("upload", { imageURL: data.Location });
+  });
 });
 
 const port = 5000;
